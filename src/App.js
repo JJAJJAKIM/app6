c4df43f9fee7470f41b8aa64ab4fbea965548815
JJAJJAKIM <csm9121@gmail.com>
2024-09-24 16:41:56 +0900

    파일업로드 프론트서버 리액트로 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UploadImg from './containers/UploadImg';
+import ErrorPage from './containers/ErrorPage';
+import axios from 'axios';
+import {useState} from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    { <UploadImg />}
+    </>
   );
 }
 
